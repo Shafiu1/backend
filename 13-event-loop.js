c5170a9d70df1fb1ee41989d755cb804dd1349a6
Shafiul Alam <shafiulalam0307@gmail.com>
2025-08-23 12:01:04 +0600

    event loop understanding

---
+++ 13-event-loop.js
@@ -0,0 +1,19 @@
+// const {readFile} = require('fs');
+
+// console.log('startin the first task');
+
+// readFile('./content/first.txt','utf8',(err,res)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(res);
+//     console.log('completed the first task');
+// })
+
+// console.log('Starting the next task');
+
+// setInterval(() => {
+//     console.log('hello world');
+// }, 2000);
+// console.log('I will run first');
