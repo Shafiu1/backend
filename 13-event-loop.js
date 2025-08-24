ed568f2eaec93d6102ba7759e75b24f7f593e0ee
Shafiul Alam <shafiulalam0307@gmail.com>
2025-08-24 14:28:07 +0600

    async pattern best practice

--- 13-event-loop.js
+++ 13-event-loop.js
@@ -1,19 +1,19 @@
-// const {readFile} = require('fs');
+const {readFile} = require('fs');
 
-// console.log('startin the first task');
+console.log('startin the first task');
 
-// readFile('./content/first.txt','utf8',(err,res)=>{
-//     if(err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(res);
-//     console.log('completed the first task');
-// })
+readFile('./content/first.txt','utf8',(err,res)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(res);
+    console.log('completed the first task');
+})
 
-// console.log('Starting the next task');
+console.log('Starting the next task');
 
-// setInterval(() => {
-//     console.log('hello world');
-// }, 2000);
-// console.log('I will run first');
+setInterval(() => {
+    console.log('hello world');
+}, 2000);
+console.log('I will run first');
